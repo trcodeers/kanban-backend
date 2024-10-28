a5adf909eeba81737cba12779d8f461d42cd2ad6
Tarun-kumar-singh <tarunks2512@gmail.com>
2024-10-28 18:40:50 +0530

    format

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ app.use(cors({
 
 app.use(express.json());
 app.use('/task', taskRoute);
+
 mongoose.connect(
     process.env.mongoURL
-    )
+)
     .then(() => {
         console.log('Connected to MongoDB');
-});
+    });
 
 app.listen(5000, () => {
     console.log('Server is running on the port 5000')
